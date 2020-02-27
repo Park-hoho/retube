371e9f25f6a876f073ee157146ed02ee602308d5
parkbh <qudgh4290@gmail.com>
2020-02-27 22:25:41 +0900

    sever set

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-console.log("hi");
-console.log("hi");
-console.log("hi");
+const express = require('express'); //node_modules 에서 Import
+const app = express();
+
+const PORT = 4000;
+
+function handleListening(){
+    console.log(`Listening on: http://localhost:${PORT}`)
+}
+
+app.listen(PORT, handleListening); //4000포트 연결
