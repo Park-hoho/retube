da05a51823bb900a04ae7cfc03b48293843ff590
parkbh <qudgh4290@gmail.com>
2020-02-29 22:28:13 +0900

    add locals variable

--- middlewares.js
+++ middlewares.js
@@ -1,7 +1,8 @@
 import routes from "./routes";
-
-export const localsMiddleware = (req, res, next) => { //마지막에 next(); 꼭 해주자
+//전역적으로 사용할 수 있는 변수를 추가하는 방법
+//템플릿, 뷰, 모든 곳에서 사용가능함
+export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "ReTube";
     res.locals.routes = routes;
-    next();
+    next(); //마지막에 next(); 꼭 해주자
 };
