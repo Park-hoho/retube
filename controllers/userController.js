10080e959afdc22ae2d4bb8861c5848c1de428b4
parkbh <qudgh4290@gmail.com>
2020-03-10 23:19:32 +0900

    passport2

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,27 @@
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 }; // render 첫번째 인자는 pug 이름
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   const {
     body: { name, email, password, password2 }
   } = req;
-  if(password !== password2) {
+  if (password !== password2) {
     res.status(400);
     res.render("join", { pageTitle: "Join" });
   } else {
     // To Do: Register User
+    try {
+      const user = await User({
+        name,
+        email
+      });
+      await User.register(user, password);
+    } catch (error) {
+      console.log(error);
+    }
     // To Do: Log user in
     res.redirect(routes.home);
   }
@@ -28,8 +38,11 @@ export const logout = (req, res) => {
   res.redirect(routes.home);
 };
 
-export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "User Detail" });
 
-export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
 
-export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
