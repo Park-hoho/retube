a799393196b51ef01f0572f27378e99f297c7a97
parkbh <qudgh4290@gmail.com>
2020-03-10 21:36:37 +0900

    passport

---
+++ passport.js
@@ -0,0 +1,7 @@
+import passport from "passport";
+import User from "./models/User";
+// strategy는 로그인 하는 방식
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser);
