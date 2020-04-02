440195d51e5a4b4cf7a6257ee8837f8270962395
parkbh <qudgh4290@gmail.com>
2020-04-02 23:48:00 +0900

    Title : Mute Unmute / Enter Fullscreen Exit Fullscreen

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,22 +1,39 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
-const playBtn = document.getElementById("#jsPlayButton");
-
-console.log("jo");
-console.log(videoContainer);
-console.log(videoPlayer);
-console.log(playBtn);
+const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeBtn");
+const fullScrnBtn = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
     videoPlayer.pause();
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
   }
+}
+
+function handleVolumeClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+  }
+}
+
+function goFullSCreen() {
+  videoPlayer.requestFullscreen();
+  fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScrnBtn.removeEventListener("click", goFullSCreen);
 }
 
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
+  volumeBtn.addEventListener("click", handleVolumeClick);
+  fullScrnBtn.addEventListener("click", goFullSCreen);
 }
 
 if (videoContainer) {
